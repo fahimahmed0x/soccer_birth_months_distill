24584802162434af177375e1f125b7f276a390eb
fahimahmed0x <cpcheats1991@gmail.com>
2021-06-21 21:13:55 -0400

    Updated graph

--- make-birth-plot.R
+++ make-birth-plot.R
@@ -8,12 +8,13 @@ birthdates <- read_csv("birthdates.csv",
 birth_plot <- birthdates %>%
   mutate(birth_date = str_sub(value, 2, 11)) %>%
   mutate(birth_date = parse_date(birth_date)) %>%
-  mutate(month = month(birth_date)) %>%
+  mutate(raw_month = months(birth_date)) %>%
+  mutate(month = parse_factor(raw_month, levels = month.name)) %>%
   group_by(month) %>%
   summarize(total = n()) %>%
-  ggplot(mapping = aes(x = month, y = total)) +
+  ggplot(mapping = aes(x = fct_rev(month), y = total)) +
   geom_col() +
-  scale_x_continuous(breaks = 1:12) +
+  coord_flip() +
   theme_classic() +
   labs(title = "Birth Months of UEFA Euro 2020 Soccer Players",
        subtitle = "Most of the UEFA Euro 2020 soccer players are born in March",
